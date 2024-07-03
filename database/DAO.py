c106894db517f3d9bee38070592a2bd0f23caecb
Cosimo Sanna <s299120@studenti.polito.it>
2024-07-03 16:34:18 +0200

    VERSIONE 3.0

--- database/DAO.py
+++ database/DAO.py
@@ -24,9 +24,12 @@ class DAO():
     def getEdges(year, days, idMap):
         cnx = DBConnect.get_connection()
         cursor1 = cnx.cursor()
-        query1 = """select n.state1, n.state2, count(*) as peso  from neighbor n, sighting s1, sighting s2
-                    where  year(s1.`datetime`) = %s and year(s2.`datetime`) = %s and s2.state = n.state2 and n.state1 < n.state2 
-                    and s1.state = n.state1 and ABS(datediff(s1.`datetime`, s2.`datetime`)) <= %s group by n.state1, n.state2"""
+        query1 = """select n.state1, n.state2, count(*) as peso  from neighbor n left join sighting s1 
+                    on  year(s1.`datetime`) = %s and s1.state = n.state1
+                    left join sighting s2 on year(s2.`datetime`) = %s and s2.state = n.state2
+                    and ABS(datediff(s1.`datetime`, s2.`datetime`)) <= %s 
+                    where n.state1 < n.state2 
+                    group by n.state1, n.state2"""
         cursor1.execute(query1, (year, year, days))
         rows = cursor1.fetchall()
         result = []
